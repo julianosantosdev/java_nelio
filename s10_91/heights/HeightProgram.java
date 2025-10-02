c2b0bd347e96b2258846459bc1b16528885ecace
Juliano Santos <julianosantosdev@outlook.com>
2025-10-02 20:12:25 -0300

    add:s10-91 exercise

---
+++ s10_91/heights/HeightProgram.java
@@ -0,0 +1,60 @@
+package heights;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class HeightProgram {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Quantas pessoas serão digitadas? ");
+		int n = scanner.nextInt();
+		scanner.nextLine();
+		
+		Person[] peopleList = new Person[n];
+		
+		double heightSum = 0;
+		int lessThan16 = 0;
+		
+		
+		for (int i = 0; i < peopleList.length; i++) {
+			System.out.printf("Dados da %d pessoa: ", i+1);
+			System.out.println();
+			
+			System.out.print("Nome: ");
+			String name = scanner.nextLine();
+			
+			System.out.print("Idade: ");
+			int age = scanner.nextInt();
+			scanner.nextLine();
+			
+			System.out.print("Altura: ");
+			double height = scanner.nextDouble();
+			scanner.nextLine();
+			System.out.println();
+			
+			peopleList[i] = new Person(name, age, height);
+		}
+		
+		for (int i = 0; i < peopleList.length; i++) {
+			heightSum += peopleList[i].getHeight();
+			
+			if (peopleList[i].getAge() <= 14) {
+				lessThan16++;
+			}			
+		}
+		
+		System.out.println();
+		System.out.printf("Altura Média: %.2f\n", heightSum/peopleList.length);
+		System.out.printf("Pessoas com menos de 16 anos: %.2f %%\n", ((double)lessThan16/peopleList.length)*100);
+		
+		for (int i = 0; i < peopleList.length; i++) {
+			if (peopleList[i].getAge() < 16) {
+				System.out.println(peopleList[i].getName());
+			}
+		}
+	}
+
+}
---
+++ s10_91/heights/Person.java
@@ -0,0 +1,40 @@
+package heights;
+
+public class Person {
+	private String name;
+	private int age;
+	private double height;
+	
+	public Person (String name, int age, double height) {
+		setName(name);
+		setAge(age);
+		setHeight(height);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+
+
+}
